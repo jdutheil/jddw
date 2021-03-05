27c786c5cdbd4766e7194fd5eded515666eac8ea
Jérémy Dutheil <dutheil.jeremy@gmail.com>
2021-03-05 18:34:12 +0100

    homescreen beginning

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,11 @@
 import logo from './logo.svg'
-import './App.css'
+
+import Home from './components/home/Home'
+
+import './App.scss'
 
 function App() {
-  return <div className='App'>MyApp </div>
+  return <Home />
 }
 
 export default App
